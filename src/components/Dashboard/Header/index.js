6bf62d92d9d58af62a5468df85785af52f325602
Lucas <freirejandre@gmail.com>
2019-09-08 15:04:50 -0300

    dasda

--- src/components/Dashboard/Header/index.js
+++ src/components/Dashboard/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SimpleTopAppBar, TopAppBarFixedAdjust } from "@rmwc/top-app-bar";
-import '@material/top-app-bar/dist/mdc.top-app-bar.css';
+// import '@material/top-app-bar/dist/mdc.top-app-bar.css';
 
 
 
