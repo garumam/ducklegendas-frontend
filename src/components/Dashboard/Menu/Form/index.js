007d789fdc88184265b46d8ee069d04df4afe2a7
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-09-09 19:27:13 -0300

    erro resolvido dom property

--- src/components/Dashboard/Menu/Form/index.js
+++ src/components/Dashboard/Menu/Form/index.js
@@ -4,7 +4,7 @@ import {Col, CustomForm} from './styles';
 
 export const InputPersonalizado = (props) => (
     <Col>
-      {props.type !=="submit" ? <label htmlFor={props.name}>{props.labelText}</label> :""}
+      {props.type !=="submit" ? <label htmlFor={props.name}>{props['data-label']}</label> :""}
      
       { <Field {...props}/> }
     </Col>
@@ -25,10 +25,10 @@ const formikConfig = withFormik({
 export default formikConfig(() => ( 
     
         <CustomForm className="formulario"> 
-            <InputPersonalizado labelText="E-mail" name="email" type="email" />
-            <InputPersonalizado labelText="Senha" name="password" type="password" />
-            <InputPersonalizado labelText="Nome" name="nome" type="text" />
-            <InputPersonalizado labelText="Mensagem" name="mensagem" type="text" />
+            <InputPersonalizado data-label="E-mail" name="email" type="email" />
+            <InputPersonalizado data-label="Senha" name="password" type="password" />
+            <InputPersonalizado data-label="Nome" name="nome" type="text" />
+            <InputPersonalizado data-label="Mensagem" name="mensagem" type="text" />
             <InputPersonalizado type="submit" value="Enviar" />
         </CustomForm>
     )
