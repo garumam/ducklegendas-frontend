27836714ba8def12415edba509662e254c4b7bb0
Lucas <freirejandre@gmail.com>
2019-09-22 01:07:56 -0300

    axios react

--- src/components/Front/Login/index.js
+++ src/components/Front/Login/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import {Login} from "./styles";
+import {Link} from 'react-router-dom';
 import { InputPersonalizado } from "../Contato";
+import api from '../../../services/api';
 var estilo = {
   paddingTop: "6.38rem"
 };
 export default props => {
-  function logar(e) {
+  async function logar(e) {
     e.preventDefault();
+    await api.post('/login', {email:'teste@teste.com',password:'159159'}).then(r=>{
+      console.log(r.data.message)
+    }).catch(e=>{
+      console.log(e.response.data.message)
+    })
+    
   }
   return (
     <div style={estilo} className="container flex-center">
@@ -27,6 +35,8 @@ export default props => {
               value="Logar"
               onClick={e => logar(e)}
             />
+            <Link to='/esqueceu'>Esqueceu a senha ?</Link>
+            <Link to='/registro'>Criar conta </Link>
           </form>
         </div>
       </div>
