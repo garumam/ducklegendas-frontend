3146172abd780f2907023de2a0abc9e890172e36
Lucas <freirejandre@gmail.com>
2019-09-25 21:09:37 -0300

    erros object

--- src/components/Front/ResetarSenha/index.js
+++ src/components/Front/ResetarSenha/index.js
@@ -1,102 +1,114 @@
 import React from "react";
-import {Login,Error} from "./styles";
-import {withRouter} from 'react-router-dom';
+import { Login, Error } from "./styles";
+import { withRouter } from "react-router-dom";
 import { InputPersonalizado } from "../Contato";
-import api from '../../../services/api';
+import api from "../../../services/api";
 var estilo = {
   paddingTop: "6.38rem"
 };
 const ResetarSenha = props => {
-  const [errors,setErrors] = React.useState(null);
-  const [input,setInput] = React.useReducer( (state, newState) => ({...state, ...newState}),
+  const [errors, setErrors] = React.useState(null);
+  const [input, setInput] = React.useReducer(
+    (state, newState) => ({ ...state, ...newState }),
     {
-      email: '',
-      password: '',
-      password_confirmation: ''
+      email: "",
+      password: "",
+      password_confirmation: ""
     }
   );
-  const {token} = props.match.params;
-  
+  const { token } = props.match.params;
+
   async function resetPassword(e) {
     e.preventDefault();
-    
-    const values = {email: input.email};
-    if(token){
+
+    const values = { email: input.email };
+    if (token) {
       values.token = token;
       values.password = input.password;
       values.password_confirmation = input.password_confirmation;
-    }else{
+    } else {
       values.urlFront = window.location.href;
     }
 
-    const uriApi = token? '/password/reset' : '/password/create';
+    const uriApi = token ? "/password/reset" : "/password/create";
 
-    await api.post(uriApi, values)
-    .then(r=>{
-      console.log(r.data)
-      setErrors(r.data.success);
-    }).catch(e=>{
-
-      if (e.response === undefined) { // NETWORK ERROR
-        console.log('Sem conexão');
-        setErrors('Problema de conexão com o servidor, tente mais tarde!');
-      }else{
-        console.log(e.response.data.error);
-        setErrors(e.response.data.error);
-      }
-
-    })
-    
+    await api
+      .post(uriApi, values)
+      .then(r => {
+        console.log(r.data);
+        setErrors(r.data.success);
+      })
+      .catch(e => {
+        if (e.response === undefined) {
+          // NETWORK ERROR
+          console.log("Sem conexão");
+          setErrors("Problema de conexão com o servidor, tente mais tarde!");
+        } else {
+          console.log(e.response.data.error);
+          setErrors(e.response.data.error);
+        }
+      });
   }
 
-  function handleInputChange(e){
-    const { name, value} = e.target;
-    setInput({ [name]: value});
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setInput({ [name]: value });
     console.log(input);
   }
 
   return (
     <div style={estilo} className="container flex-center">
-    <Login className="card card-shadow">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="header-card">
-              <h2>{props.title}</h2>
+      <Login className="card card-shadow">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="header-card">
+                <h2>{props.title}</h2>
+              </div>
             </div>
+            <div className="card-border" />
+            <form className="formulario">
+              <InputPersonalizado
+                title="E-mail"
+                name="email"
+                type="email"
+                value={input.email}
+                onChange={handleInputChange}
+              />
+              {token && (
+                <>
+                  <InputPersonalizado
+                    title="Senha"
+                    name="password"
+                    type="password"
+                    value={input.password}
+                    onChange={handleInputChange}
+                  />
+                  <InputPersonalizado
+                    title="Confirmação de senha"
+                    name="password_confirmation"
+                    type="password"
+                    value={input.password_confirmation}
+                    onChange={handleInputChange}
+                  />
+                </>
+              )}
+
+              <InputPersonalizado
+                type="submit"
+                value="Resetar"
+                onClick={e => resetPassword(e)}
+              />
+              <Error>
+                {errors &&
+                  Object.keys(errors).map(key => {
+                    return <span>{errors[key]}</span>
+                  })}
+              </Error>
+            </form>
           </div>
-          <div className="card-border" />
-          <form className="formulario">
-            <InputPersonalizado title="E-mail" name="email" type="email" 
-              value={input.email} 
-              onChange={handleInputChange} 
-            />
-            {token && 
-            <>
-            <InputPersonalizado title="Senha" name="password" type="password" 
-            value={input.password} 
-            onChange={handleInputChange} 
-            />
-            <InputPersonalizado title="Confirmação de senha" name="password_confirmation" type="password" 
-              value={input.password_confirmation} 
-              onChange={handleInputChange} 
-            /> 
-            </>
-            }
-            
-            <InputPersonalizado
-              type="submit"
-              value="Resetar"
-              onClick={e => resetPassword(e)}
-            />
-            <Error>
-            {errors && <span>{errors}</span>}
-            </Error>
-           
-          </form>
         </div>
-      </div>
-    </Login>
+      </Login>
     </div>
   );
 };
