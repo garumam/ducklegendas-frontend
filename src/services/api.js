5dd399640837ec20f07f038676db285ede421c89
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-11-07 01:58:55 -0300

    ajuste para https

--- src/services/api.js
+++ src/services/api.js
@@ -39,7 +39,7 @@ export const getRequest = (uri) => api.get(uri);
 
 export const refreshAuthorization = () => {api.defaults.headers.Authorization = isAuthenticated();}
 
-export const baseUrl = 'http://ducklegendas.com/api/public/';
+export const baseUrl = 'https://ducklegendas.com/api/public/';
 //export const baseUrl = 'http://127.0.0.1:8000/';
 
 const api = axios.create({
