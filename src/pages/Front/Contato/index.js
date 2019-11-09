f095575833e681152344c07436849d41bb9d2009
Felipe Ribeiro <felipe.epr.faculdade@gmail.com>
2019-11-09 17:34:04 -0300

    recaptcha e link de request antigo

--- src/pages/Front/Contato/index.js
+++ src/pages/Front/Contato/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useRef, useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import { postRequest } from "services/api";
 import {Contato} from "./styles.js";
 
@@ -14,21 +15,26 @@ export default props => {
   const inputNome = useRef(null);
   const inputEmail = useRef(null);
   const inputMensagem = useRef(null);
+  const recaptchaRef = useRef(null);
   const [message, setMessage] = useState(null);
 
   const submit = async (e) => {
     e.preventDefault();
-    const values = {
-      nome: inputNome.current.value,
-      email: inputEmail.current.value,
-      mensagem: inputMensagem.current.value
-    }
-    setMessage('Aguarde um momento...');
-    const res = await postRequest('contact', values);
-    if(res.success){
-      setMessage(res.success);
+    if(recaptchaRef.current && recaptchaRef.current.getValue()){
+      const values = {
+        nome: inputNome.current.value,
+        email: inputEmail.current.value,
+        mensagem: inputMensagem.current.value
+      }
+      setMessage('Aguarde um momento...');
+      const res = await postRequest('contact', values);
+      if(res.success){
+        setMessage(res.success);
+      }else{
+        setMessage(res.error);
+      }
     }else{
-      setMessage(res.error);
+      setMessage('Confirmação do recaptcha obrigatória!');
     }
   }
   return (
@@ -51,6 +57,11 @@ export default props => {
             <InputPersonalizado refs={inputNome} title ="Nome" name="nome" type="text" />
             <InputPersonalizado refs={inputEmail} title ="E-mail" name="email" type="email" />
             <InputPersonalizado refs={inputMensagem} title ="Mensagem" name="mensagem" type="textarea" />
+            <ReCAPTCHA
+              style={{margin:'0.5rem auto 1rem auto'}}
+              sitekey="6Lex5cEUAAAAAFbTCze9OTDs9IWlWUi3fxP5-nhv"
+              ref={recaptchaRef}
+            />
             <InputPersonalizado type="submit" value="Enviar" />
           </form>
         </div>
