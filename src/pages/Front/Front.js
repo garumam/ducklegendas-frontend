619d4ce273a1c8806372d56c25444c899a2afcba
Lucas <freirejandre@gmail.com>
2019-11-10 16:23:02 -0300

    REFATORANDO E REMOVENDO BOOTSTRAP

--- src/pages/Front/Front.js
+++ src/pages/Front/Front.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import { getRequest } from "services/api";
 import Doacao from "./Doacao";
 import {LegendasAndamento} from "./LegendasAndamento";
+import {Container,Row} from "./styles";
 import Ranking from "./Ranking";
 import Publicidade from "./Publicidade";
 
@@ -24,25 +25,24 @@ export default props => {
 
   const location = useLocation().pathname.replace('/','');
   return(
-  <>
-    <div  className="container paddingTop">
+  
+    <Container>
       {messages.map((item, index) => (
         <div key={index} className={`alert alert-${item.type}`}>
           {item.message}
         </div>
       ))}
-      <div className="row">
-        <div className="col-sm-12 col-md-12 col-lg-8">
-          {props.children}
-          <Publicidade title="Publicidade" />
-        </div>
-        <div className="col-sm-12 col-md-12 col-lg-4">
-          <Doacao title="Doação" />
-          <LegendasAndamento title="Próximas Legendas" />
-          {location !== 'ranking' && <Ranking title="Ranking" />}
-        </div>
-      </div>
-    </div>
-  </>
+      <Row>
+        <div className="col-sm-12 col-md-12 col-lg-8"> 
+            {props.children}
+            <Publicidade title="Publicidade" />
+        </div> 
+          <div className="col-sm-12 col-md-12 col-lg-4"> 
+            <Doacao title="Doação" />
+            <LegendasAndamento title="Próximas Legendas" />
+            {location !== 'ranking' && <Ranking title="Ranking" />}
+        </div> 
+       </Row> 
+    </Container>
   )
 };
---
+++ src/pages/Front/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    max-width:1200px;
+    width:100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+    padding-top: 7rem;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+`;
